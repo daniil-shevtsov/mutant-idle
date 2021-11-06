4eb14c9de92118981b703a2b8464c29fa7cb542c
Daniil Shevtsov <me@daniilshevtsov.com>
2021-11-06 14:21:39 +0300

    Add style for resource composable

--- app/src/main/java/com/daniil/shevtsov/idle/main/ui/resource/ResourceComposable.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/main/ui/resource/ResourceComposable.kt
@@ -1,9 +1,18 @@
 package com.daniil.shevtsov.idle.main.ui.resource
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.daniil.shevtsov.idle.main.ui.Pallete
 import com.daniil.shevtsov.idle.main.ui.ResourceModel
 
 @Preview
@@ -21,8 +30,14 @@ fun ResourcePreview() {
 fun ResourceComposable(
     resource: ResourceModel,
 ) {
-    Row {
-        Text(text = resource.name)
-        Text(text = resource.value)
+    Row(
+        modifier = Modifier
+            .background(Pallete.Red)
+            .padding(4.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        Text(text = resource.name,fontSize = 24.sp, color = Color.White)
+        Text(text = resource.value,fontSize = 16.sp, modifier = Modifier.background(Color.White).padding(4.dp))
     }
 }
