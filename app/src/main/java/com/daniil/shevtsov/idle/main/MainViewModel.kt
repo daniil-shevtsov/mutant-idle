226a6e9d988c47983af4bed260aea8d1019c9a64
Daniil Shevtsov <me@daniilshevtsov.com>
2021-11-07 19:54:34 +0300

    Add hardcoded actions

--- app/src/main/java/com/daniil/shevtsov/idle/main/MainViewModel.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/main/MainViewModel.kt
@@ -8,6 +8,8 @@ import com.daniil.shevtsov.idle.main.domain.upgrade.ObserveUpgradesUseCase
 import com.daniil.shevtsov.idle.main.domain.upgrade.Upgrade
 import com.daniil.shevtsov.idle.main.domain.upgrade.UpgradeStatus
 import com.daniil.shevtsov.idle.main.ui.MainViewState
+import com.daniil.shevtsov.idle.main.ui.actions.ActionModel
+import com.daniil.shevtsov.idle.main.ui.actions.ActionPane
 import com.daniil.shevtsov.idle.main.ui.actions.ActionsState
 import com.daniil.shevtsov.idle.main.ui.resource.ResourceModelMapper
 import com.daniil.shevtsov.idle.main.ui.shop.ShopState
@@ -45,7 +47,7 @@ class MainViewModel @Inject constructor(
                             )
                         }
                         .sortedBy {
-                            when(it.status) {
+                            when (it.status) {
                                 UpgradeStatusModel.Affordable -> 0
                                 UpgradeStatusModel.NotAffordable -> 1
                                 UpgradeStatusModel.Bought -> 2
@@ -72,7 +74,24 @@ class MainViewModel @Inject constructor(
     private fun initViewState(): MainViewState = MainViewState.Loading
 
     private fun createActionState() = ActionsState(
-        actionPanes = emptyList()
+        actionPanes = listOf(
+            ActionPane(
+                actions = listOf(
+                    ActionModel(id = 0L, title = "Work"),
+                    ActionModel(id = 1L, title = "Buy a pet"),
+                    ActionModel(id = 2L, title = "Eat food"),
+                    ActionModel(id = 3L, title = "Pay rent"),
+                )
+            ),
+            ActionPane(
+                actions = listOf(
+                    ActionModel(id = 100L, title = "Work"),
+                    ActionModel(id = 101L, title = "Eat a pet"),
+                    ActionModel(id = 102L, title = "Capture man"),
+                    ActionModel(id = 103L, title = "Eat man"),
+                )
+            ),
+        )
     )
 
     private fun Upgrade.mapStatus(resource: Double): UpgradeStatusModel {
