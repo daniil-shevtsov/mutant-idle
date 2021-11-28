1aa368a78ee319de44de4a13a45ea12d581a79c0
Daniil Shevtsov <me@daniilshevtsov.com>
2021-11-28 11:21:46 +0300

    First purchase test passes

--- app/src/main/java/com/daniil/shevtsov/idle/main/domain/purchase/PurchaseBehavior.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/main/domain/purchase/PurchaseBehavior.kt
@@ -3,7 +3,8 @@ package com.daniil.shevtsov.idle.main.domain.purchase
 import com.daniil.shevtsov.idle.main.domain.resource.Resource
 import com.daniil.shevtsov.idle.main.domain.upgrade.Price
 import com.daniil.shevtsov.idle.main.domain.upgrade.Upgrade
+import com.daniil.shevtsov.idle.main.domain.upgrade.UpgradeStatus
 
 fun buyUpgrade(upgrade: Upgrade, forPrice: Price, whileResourceIs: Resource): Upgrade {
-    return upgrade
+    return upgrade.copy(status = UpgradeStatus.Bought)
 }
