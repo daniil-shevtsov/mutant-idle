827ba0c69f61c50f9c56a2ffeef8dea11a612a25
Daniil Shevtsov <me@daniilshevtsov.com>
2021-11-28 11:24:44 +0300

    Second purchase test passes

--- app/src/main/java/com/daniil/shevtsov/idle/main/domain/purchase/PurchaseBehavior.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/main/domain/purchase/PurchaseBehavior.kt
@@ -1,10 +1,13 @@
 package com.daniil.shevtsov.idle.main.domain.purchase
 
 import com.daniil.shevtsov.idle.main.domain.resource.Resource
-import com.daniil.shevtsov.idle.main.domain.upgrade.Price
 import com.daniil.shevtsov.idle.main.domain.upgrade.Upgrade
 import com.daniil.shevtsov.idle.main.domain.upgrade.UpgradeStatus
 
-fun buyUpgrade(upgrade: Upgrade, forPrice: Price, whileResourceIs: Resource): Upgrade {
-    return upgrade.copy(status = UpgradeStatus.Bought)
+fun buyUpgrade(upgrade: Upgrade, currentResource: Resource): Upgrade {
+    val newStatus = when {
+        upgrade.price.value <= currentResource.value -> UpgradeStatus.Bought
+        else -> UpgradeStatus.NotBought
+    }
+    return upgrade.copy(status = newStatus)
 }
--- app/src/test/java/com/daniil/shevtsov/idle/main/domain/purchase/PurchaseBehaviorTest.kt
+++ app/src/test/java/com/daniil/shevtsov/idle/main/domain/purchase/PurchaseBehaviorTest.kt
@@ -14,8 +14,15 @@ internal class PurchaseBehaviorTest {
     @Test
     fun `should buy upgrade if it's affordable`() {
         val upgrade = upgrade(price = 2.0, status = UpgradeStatus.NotBought)
-        val result = buyUpgrade(upgrade, forPrice = upgrade.price, whileResourceIs = Resource(4.0))
+        val result = buyUpgrade(upgrade = upgrade, currentResource = Resource(4.0))
         assertThat(result).prop(Upgrade::status).isEqualTo(UpgradeStatus.Bought)
+    }
+
+    @Test
+    fun `should not buy upgrade if it's not affordable`() {
+        val upgrade = upgrade(price = 10.0, status = UpgradeStatus.NotBought)
+        val result = buyUpgrade(upgrade = upgrade, currentResource = Resource(4.0))
+        assertThat(result).prop(Upgrade::status).isEqualTo(UpgradeStatus.NotBought)
     }
 
 }
