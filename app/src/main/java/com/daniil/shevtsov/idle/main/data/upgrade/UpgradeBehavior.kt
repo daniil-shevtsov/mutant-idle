b59fde9949313b58a2671ab0f1520d86c2de5aa3
Daniil Shevtsov <me@daniilshevtsov.com>
2021-12-02 10:17:56 +0300

    Start tdding upgrade behavior

---
+++ app/src/main/java/com/daniil/shevtsov/idle/main/data/upgrade/UpgradeBehavior.kt
@@ -0,0 +1,11 @@
+package com.daniil.shevtsov.idle.main.data.upgrade
+
+import com.daniil.shevtsov.idle.main.domain.upgrade.Upgrade
+import kotlinx.coroutines.flow.Flow
+
+object UpgradeBehavior {
+
+    fun observeAll(storage: UpgradeStorage): Flow<List<Upgrade>> {
+        return storage.observeAll()
+    }
+}
--- app/src/test/java/com/daniil/shevtsov/idle/main/domain/upgrade/UpgradeBehaviorTest.kt
+++ app/src/test/java/com/daniil/shevtsov/idle/main/domain/upgrade/UpgradeBehaviorTest.kt
@@ -1,5 +1,37 @@
 package com.daniil.shevtsov.idle.main.domain.upgrade
 
+import app.cash.turbine.test
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.daniil.shevtsov.idle.main.data.upgrade.UpgradeBehavior
+import com.daniil.shevtsov.idle.main.data.upgrade.UpgradeStorage
+import com.daniil.shevtsov.idle.util.upgrade
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
 internal class UpgradeBehaviorTest {
+
+    private lateinit var behavior: UpgradeBehavior
+
+    @BeforeEach
+    fun onSetup() {
+        behavior = UpgradeBehavior
+    }
+
+    @Test
+    fun `when observe - then return initial upgrades`() = runBlockingTest {
+        val initialUpgrades = listOf(
+            upgrade(id = 0L),
+            upgrade(id = 1L),
+        )
+        val storage = UpgradeStorage(
+            initialUpgrades = initialUpgrades
+        )
+        behavior.observeAll(storage = storage).test {
+            assertThat(awaitItem())
+                .isEqualTo(initialUpgrades)
+        }
+    }
 
 }
