517b75606b39ae746f1d2716d397390ce7f1aa94
Daniil Shevtsov <me@daniilshevtsov.com>
2021-12-04 11:14:32 +0300

    refactoriiiing

--- app/src/main/java/com/daniil/shevtsov/idle/application/IdleGameViewModel.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/application/IdleGameViewModel.kt
@@ -1,15 +1,19 @@
 package com.daniil.shevtsov.idle.application
 
+import com.daniil.shevtsov.idle.core.BalanceConfig
 import com.daniil.shevtsov.idle.main.data.time.TimeStorage
 import com.daniil.shevtsov.idle.main.domain.resource.UpdateResourcesUseCase
 import com.daniil.shevtsov.idle.main.domain.time.ObserveTimeUseCase
 import com.daniil.shevtsov.idle.main.domain.time.StartTimeUseCase
+import com.daniil.shevtsov.idle.main.domain.time.TimeBehavior
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
+import kotlin.time.Duration
 
 class IdleGameViewModel @Inject constructor(
+    private val balanceConfig: BalanceConfig,
     private val startTime: StartTimeUseCase,
     private val observeTime: ObserveTimeUseCase,
     private val updateResources: UpdateResourcesUseCase,
@@ -20,6 +24,11 @@ class IdleGameViewModel @Inject constructor(
     fun onStart() {
         scope.launch {
             startTime()
+            TimeBehavior.startEmitingTime(
+                timeStorage = timeStorage,
+                interval = Duration.milliseconds(balanceConfig.tickRateMillis),
+                until = Duration.INFINITE,
+            )
         }
         scope.launch {
             observeTime()
--- app/src/test/java/com/daniil/shevtsov/idle/application/IdleGameViewModelTest.kt
+++ app/src/test/java/com/daniil/shevtsov/idle/application/IdleGameViewModelTest.kt
@@ -1,20 +1,31 @@
 package com.daniil.shevtsov.idle.application
 
+import com.daniil.shevtsov.idle.MainCoroutineExtension
+import com.daniil.shevtsov.idle.core.BalanceConfig
 import com.daniil.shevtsov.idle.main.data.time.TimeStorage
 import io.mockk.mockk
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(MainCoroutineExtension::class)
 internal class IdleGameViewModelTest {
     private val viewModel: IdleGameViewModel by lazy { createViewModel() }
+
+    private val balanceConfig = BalanceConfig(
+        tickRateMillis = 1L,
+        resourcePerMillisecond = 2.0,
+    )
 
     private val timeStorage = TimeStorage()
 
     @Test
-    fun kek() {
-
+    fun kek() = runBlockingTest {
+        viewModel.onStart()
     }
 
     private fun createViewModel() = IdleGameViewModel(
+        balanceConfig = balanceConfig,
         startTime = mockk(),
         observeTime = mockk(),
         updateResources = mockk(),
