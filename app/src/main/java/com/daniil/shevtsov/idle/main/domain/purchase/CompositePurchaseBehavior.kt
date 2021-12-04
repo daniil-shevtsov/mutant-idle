a1db3e769f330a0a4f3082f6d3540fcd7e9d427d
Daniil Shevtsov <me@daniilshevtsov.com>
2021-12-04 09:33:03 +0300

    Create test for composite purchase behavior

---
+++ app/src/main/java/com/daniil/shevtsov/idle/main/domain/purchase/CompositePurchaseBehavior.kt
@@ -0,0 +1,14 @@
+package com.daniil.shevtsov.idle.main.domain.purchase
+
+import com.daniil.shevtsov.idle.main.data.resource.ResourceStorage
+import com.daniil.shevtsov.idle.main.data.upgrade.UpgradeStorage
+
+object CompositePurchaseBehavior {
+    fun buyUpgrade(
+        upgradeStorage: UpgradeStorage,
+        resourceStorage: ResourceStorage,
+        upgradeId: Long
+    ) {
+
+    }
+}
---
+++ app/src/test/java/com/daniil/shevtsov/idle/main/domain/purchase/CompositePurchaseBehaviorTest.kt
@@ -0,0 +1,45 @@
+package com.daniil.shevtsov.idle.main.domain.purchase
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isNotNull
+import assertk.assertions.prop
+import com.daniil.shevtsov.idle.main.data.resource.ResourceStorage
+import com.daniil.shevtsov.idle.main.data.upgrade.UpgradeStorage
+import com.daniil.shevtsov.idle.main.domain.upgrade.Upgrade
+import com.daniil.shevtsov.idle.main.domain.upgrade.UpgradeStatus
+import com.daniil.shevtsov.idle.util.upgrade
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.jupiter.api.Test
+
+internal class CompositePurchaseBehaviorTest {
+
+    private val behavior = CompositePurchaseBehavior
+
+    @Test
+    fun `when buying affordable upgrade - then update it and resource`() = runBlockingTest {
+        val upgradeId = 1L
+        val resourceStorage = ResourceStorage()
+        val upgradeStorage = UpgradeStorage(
+            initialUpgrades = listOf(
+                upgrade(id = upgradeId, price = 25.0)
+            )
+        )
+        resourceStorage.setNewValue(75.0)
+
+        behavior.buyUpgrade(
+            upgradeStorage = upgradeStorage,
+            resourceStorage = resourceStorage,
+            upgradeId = upgradeId,
+        )
+
+        assertThat(upgradeStorage.getUpgradeById(id = upgradeId))
+            .isNotNull()
+            .prop(Upgrade::status)
+            .isEqualTo(UpgradeStatus.Bought)
+
+        assertThat(resourceStorage.getCurrentValue())
+            .isEqualTo(50.0)
+    }
+
+}
