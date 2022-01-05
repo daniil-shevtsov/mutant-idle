3a124c066124c73e0bc7706c6e1473ac580b3777
Daniil Shevtsov <me@daniilshevtsov.com>
2022-01-05 13:16:49 +0300

    Fix paddings

--- app/src/main/java/com/daniil/shevtsov/idle/feature/upgrade/view/UpgradeComposable.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/feature/upgrade/view/UpgradeComposable.kt
@@ -2,10 +2,9 @@ package com.daniil.shevtsov.idle.feature.upgrade.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.daniil.shevtsov.idle.core.ui.Pallete
+import com.daniil.shevtsov.idle.core.ui.upgradeListPreviewStub
 import com.daniil.shevtsov.idle.core.ui.upgradePreviewStub
 import com.daniil.shevtsov.idle.feature.upgrade.presentation.UpgradeModel
 import com.daniil.shevtsov.idle.feature.upgrade.presentation.UpgradeStatusModel
@@ -22,12 +22,33 @@ import com.daniil.shevtsov.idle.feature.upgrade.presentation.UpgradeStatusModel
 @Preview
 @Composable
 fun UpgradePreview() {
-    Column {
-        UpgradeStatusModel.values().forEach { status ->
-            Upgrade(upgrade = upgradePreviewStub(status = status))
-        }
-    }
+    Upgrade(upgrade = upgradePreviewStub(status = UpgradeStatusModel.Affordable))
+}
 
+@Preview(heightDp = 270)
+@Composable
+fun UpgradeListPreview() {
+    UpgradeList(upgradeList = upgradeListPreviewStub())
+}
+
+@Composable
+fun UpgradeList(
+    upgradeList: List<UpgradeModel>,
+    modifier: Modifier = Modifier,
+    onUpgradeSelected: (upgradeId: Long) -> Unit = {},
+) {
+    LazyColumn(
+        content = {
+            items(items = upgradeList) { upgrade ->
+                Upgrade(
+                    upgrade = upgrade,
+                    onClicked = { onUpgradeSelected(upgrade.id) }
+                )
+            }
+        },
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+    )
 }
 
 @Composable
@@ -46,6 +67,7 @@ fun Upgrade(
     }
     Column(
         modifier = Modifier
+            .padding(4.dp)
             .background(Pallete.Red)
             .clickable { onClicked() }
             .padding(4.dp),
