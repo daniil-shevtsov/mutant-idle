21b7f1ee9313bff980838b9a3070d740b6e2d89e
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-04-24 15:28:23 +0300

    Create stubs for drawer state

--- app/src/main/java/com/daniil/shevtsov/idle/feature/main/presentation/SectionStubs.kt
+++
@@ -1,9 +0,0 @@
-package com.daniil.shevtsov.idle.feature.main.presentation
-
-fun sectionState(
-    key: SectionKey = SectionKey.Resources,
-    isCollapsed: Boolean = false,
-) = SectionState(
-    key = key,
-    isCollapsed = isCollapsed
-)
--- app/src/main/java/com/daniil/shevtsov/idle/feature/main/view/MainScreen.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/feature/main/view/MainScreen.kt
@@ -21,12 +21,16 @@ import com.daniil.shevtsov.idle.core.ui.widgets.Cavity
 import com.daniil.shevtsov.idle.feature.action.view.ActionSection
 import com.daniil.shevtsov.idle.feature.debug.presentation.DebugComposable
 import com.daniil.shevtsov.idle.feature.debug.presentation.DebugViewAction
+import com.daniil.shevtsov.idle.feature.drawer.presentation.DrawerTabId
+import com.daniil.shevtsov.idle.feature.drawer.presentation.drawerTab
 import com.daniil.shevtsov.idle.feature.drawer.view.DrawerTabSelector
 import com.daniil.shevtsov.idle.feature.main.presentation.*
 import com.daniil.shevtsov.idle.feature.player.info.view.PlayerInfoComposable
+import com.daniil.shevtsov.idle.feature.player.job.domain.playerJobModel
 import com.daniil.shevtsov.idle.feature.ratio.view.MutantRatioPane
 import com.daniil.shevtsov.idle.feature.resource.view.ResourcePane
 import com.daniil.shevtsov.idle.feature.shop.view.Shop
+import com.daniil.shevtsov.idle.feature.tagsystem.domain.tag
 import com.google.accompanist.insets.statusBarsHeight
 
 @Preview(
@@ -34,76 +38,56 @@ import com.google.accompanist.insets.statusBarsHeight
     heightDp = 534,
 )
 @Composable
+fun MainDrawerDebugPreview() {
+    MainDrawer(
+        state = mainViewState(
+            drawerState = drawerViewState(
+                tabSelectorState = listOf(
+                    drawerTab(
+                        id = DrawerTabId.Debug,
+                        title = "Debug",
+                        isSelected = true
+                    ),
+                    drawerTab(
+                        id = DrawerTabId.PlayerInfo,
+                        title = "Player Info",
+                        isSelected = false
+                    ),
+                ),
+                drawerContent = drawerDebugContent(
+                    jobSelection = listOf(
+                        playerJobModel(
+                            title = "Lol",
+                            tags = listOf(
+                                tag(name = "lol1"),
+                                tag(name = "lol2"),
+                                tag(name = "lol3"),
+                            )
+                        ),
+                        playerJobModel(
+                            title = "Kek",
+                            tags = listOf(
+                                tag(name = "kek1"),
+                                tag(name = "kek2"),
+                                tag(name = "kek3"),
+                            )
+                        ),
+                    )
+                )
+            )
+        ),
+        onViewAction = {},
+    )
+}
+
+@Preview(
+    widthDp = 320,
+    heightDp = 534,
+)
+@Composable
 fun MainPreview() {
-    val state = viewStatePreviewStub()
     MainContent(
-        state = state,
-        onViewAction = {},
-        onActionClicked = {},
-        onUpgradeSelected = {},
-        onToggleCollapse = {}
-    )
-}
-
-@Preview(
-    widthDp = 320,
-    heightDp = 534,
-)
-@Composable
-fun MainPreviewAllCollapsed() {
-    val state = viewStatePreviewStub().copy(
-        sectionCollapse = mapOf(
-            SectionKey.Resources to true,
-            SectionKey.Actions to true,
-            SectionKey.Upgrades to true,
-        )
-    )
-    MainContent(
-        state = state,
-        onViewAction = {},
-        onActionClicked = {},
-        onUpgradeSelected = {},
-        onToggleCollapse = {}
-    )
-}
-
-@Preview(
-    widthDp = 320,
-    heightDp = 534,
-)
-@Composable
-fun MainPreviewActionsExpanded() {
-    val state = viewStatePreviewStub().copy(
-        sectionCollapse = mapOf(
-            SectionKey.Resources to true,
-            SectionKey.Actions to false,
-            SectionKey.Upgrades to true,
-        )
-    )
-    MainContent(
-        state = state,
-        onViewAction = {},
-        onActionClicked = {},
-        onUpgradeSelected = {},
-        onToggleCollapse = {}
-    )
-}
-
-@Preview(
-    widthDp = 320,
-    heightDp = 534,
-)
-@Composable
-fun MainPreviewUpgradesExpanded() {
-    val state = viewStatePreviewStub().copy(
-        sectionCollapse = mapOf(
-            SectionKey.Resources to true,
-            SectionKey.Actions to true,
-            SectionKey.Upgrades to false,
-        )
-    )
-    MainContent(
-        state = state,
+        state = viewStatePreviewStub(),
         onViewAction = {},
         onActionClicked = {},
         onUpgradeSelected = {},
@@ -171,25 +155,7 @@ fun SuccessContent(
 
     ModalDrawer(
         drawerState = drawerState,
-        drawerContent = {
-            DrawerTabSelector(
-                tabs = state.drawerState.tabSelectorState,
-                onTabSelected = { id -> onViewAction(MainViewAction.DrawerTabSwitched(id = id)) },
-            )
-            when (val drawerContentState = state.drawerState.drawerContent) {
-                is DrawerContentViewState.Debug -> {
-                    DebugComposable(state = drawerContentState.state, onAction = { action ->
-                        val mainViewAction = when (action) {
-                            is DebugViewAction.JobSelected -> MainViewAction.DebugJobSelected(action.id)
-                        }
-                        onViewAction(mainViewAction)
-                    })
-                }
-                is DrawerContentViewState.PlayerInfo -> {
-                    PlayerInfoComposable(state = drawerContentState.playerInfo)
-                }
-            }
-        },
+        drawerContent = { MainDrawer(state, onViewAction) },
         content = {
             ContentBody(
                 state = state,
@@ -199,6 +165,30 @@ fun SuccessContent(
             )
         }
     )
+}
+
+@Composable
+private fun MainDrawer(
+    state: MainViewState.Success,
+    onViewAction: (MainViewAction) -> Unit
+) {
+    DrawerTabSelector(
+        tabs = state.drawerState.tabSelectorState,
+        onTabSelected = { id -> onViewAction(MainViewAction.DrawerTabSwitched(id = id)) },
+    )
+    when (val drawerContentState = state.drawerState.drawerContent) {
+        is DrawerContentViewState.Debug -> {
+            DebugComposable(state = drawerContentState.state, onAction = { action ->
+                val mainViewAction = when (action) {
+                    is DebugViewAction.JobSelected -> MainViewAction.DebugJobSelected(action.id)
+                }
+                onViewAction(mainViewAction)
+            })
+        }
+        is DrawerContentViewState.PlayerInfo -> {
+            PlayerInfoComposable(state = drawerContentState.playerInfo)
+        }
+    }
 }
 
 @Composable
