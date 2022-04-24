742e77ad19c6bcf975e67d8f9378f49a01e486c3
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-04-24 14:12:51 +0300

    Delete all sideeffects!!!!

--- app/src/test/java/com/daniil/shevtsov/idle/feature/main/presentation/MainViewModelTest.kt
+++ app/src/test/java/com/daniil/shevtsov/idle/feature/main/presentation/MainViewModelTest.kt
@@ -6,18 +6,15 @@ import assertk.all
 import assertk.assertThat
 import assertk.assertions.*
 import com.daniil.shevtsov.idle.MainCoroutineExtension
-import com.daniil.shevtsov.idle.feature.action.data.ActionsStorage
 import com.daniil.shevtsov.idle.feature.action.domain.ActionType
 import com.daniil.shevtsov.idle.feature.action.presentation.ActionModel
 import com.daniil.shevtsov.idle.feature.action.presentation.ActionPane
 import com.daniil.shevtsov.idle.feature.action.presentation.ActionsState
-import com.daniil.shevtsov.idle.feature.debug.data.DebugConfigStorage
 import com.daniil.shevtsov.idle.feature.debug.presentation.DebugViewState
 import com.daniil.shevtsov.idle.feature.drawer.presentation.DrawerTabId
 import com.daniil.shevtsov.idle.feature.drawer.presentation.drawerTab
 import com.daniil.shevtsov.idle.feature.main.data.MainImperativeShell
 import com.daniil.shevtsov.idle.feature.main.domain.mainFunctionalCoreState
-import com.daniil.shevtsov.idle.feature.player.core.data.PlayerStorage
 import com.daniil.shevtsov.idle.feature.player.info.presentation.PlayerInfoState
 import com.daniil.shevtsov.idle.feature.player.job.domain.Butcher
 import com.daniil.shevtsov.idle.feature.player.job.domain.Mortician
@@ -32,28 +29,18 @@ import com.daniil.shevtsov.idle.feature.resource.domain.resource
 import com.daniil.shevtsov.idle.feature.resource.presentation.ResourceModel
 import com.daniil.shevtsov.idle.feature.shop.presentation.ShopState
 import com.daniil.shevtsov.idle.feature.tagsystem.domain.*
-import com.daniil.shevtsov.idle.feature.upgrade.data.UpgradeStorage
 import com.daniil.shevtsov.idle.feature.upgrade.presentation.UpgradeModel
 import com.daniil.shevtsov.idle.feature.upgrade.presentation.UpgradeStatusModel
 import com.daniil.shevtsov.idle.util.action
 import com.daniil.shevtsov.idle.util.balanceConfig
 import com.daniil.shevtsov.idle.util.player
 import com.daniil.shevtsov.idle.util.upgrade
-import io.mockk.clearAllMocks
 import kotlinx.coroutines.test.runBlockingTest
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
 @ExtendWith(MainCoroutineExtension::class)
 internal class MainViewModelTest {
-
-    private var upgradeStorage = UpgradeStorage(
-        initialUpgrades = emptyList()
-    )
-    private var actionsStorage = ActionsStorage(
-        initialActions = emptyList()
-    )
 
     private val resourcesStorage = ResourcesStorage(
         initialResources = listOf(
@@ -62,21 +49,9 @@ internal class MainViewModelTest {
         )
     )
 
-    private val resourceSpentForFullMutant = 100.0
     private val imperativeShell = MainImperativeShell(initialState = mainFunctionalCoreState())
 
-    private val playerStorage = PlayerStorage(
-        initialPlayer = player()
-    )
-
-    private val debugConfigStorage = DebugConfigStorage(initial = emptyList())
-
     private val viewModel: MainViewModel by lazy { createViewModel() }
-
-    @BeforeEach
-    fun onSetup() {
-        clearAllMocks()
-    }
 
     @Test
     fun `should form correct initial state`() = runBlockingTest {
@@ -105,21 +80,6 @@ internal class MainViewModelTest {
                     sectionState(key = SectionKey.Actions, isCollapsed = false),
                     sectionState(key = SectionKey.Upgrades, isCollapsed = false),
                 )
-            )
-        )
-
-        upgradeStorage = UpgradeStorage(
-            initialUpgrades = listOf(
-                upgrade(id = 0L),
-                upgrade(id = 1L, price = 25.0),
-                upgrade(id = 2L, price = 150.0),
-                upgrade(id = 3L, price = 10.0),
-            )
-        )
-        actionsStorage = ActionsStorage(
-            initialActions = listOf(
-                action(id = 0L, title = "human action", actionType = ActionType.Human),
-                action(id = 1L, title = "mutant action", actionType = ActionType.Mutant),
             )
         )
 
@@ -167,9 +127,6 @@ internal class MainViewModelTest {
             )
         )
 
-        setInitialResourceValue(10.0)
-        upgradeStorage = UpgradeStorage(initialUpgrades = listOf(upgrade(id = 0L, price = 5.0)))
-
         viewModel.state.test {
             val state = expectMostRecentItem()
             assertThat(state)
@@ -188,10 +145,6 @@ internal class MainViewModelTest {
                     resources = listOf(resource(key = ResourceKey.Blood, value = 10.0)),
                 )
             )
-
-            setInitialResourceValue(10.0)
-            upgradeStorage =
-                UpgradeStorage(initialUpgrades = listOf(upgrade(id = 0L, price = 20.0)))
 
             viewModel.state.test {
                 val state = expectMostRecentItem()
@@ -214,9 +167,6 @@ internal class MainViewModelTest {
                 resources = listOf(resource(key = ResourceKey.Blood, value = 10.0)),
             )
         )
-
-        setInitialResourceValue(10.0)
-        upgradeStorage = UpgradeStorage(initialUpgrades = listOf(upgrade(id = 0L, price = 10.0)))
 
         viewModel.handleAction(MainViewAction.UpgradeSelected(id = 0L))
 
@@ -243,9 +193,6 @@ internal class MainViewModelTest {
             )
         )
 
-        setInitialResourceValue(10.0)
-        upgradeStorage = UpgradeStorage(initialUpgrades = listOf(upgrade(id = 0L, price = 10.0)))
-
         viewModel.handleAction(MainViewAction.UpgradeSelected(id = 0L))
 
         viewModel.state.test {
@@ -258,16 +205,6 @@ internal class MainViewModelTest {
 
     @Test
     fun `should update mutant ratio names correctly`() = runBlockingTest {
-        upgradeStorage = UpgradeStorage(
-            initialUpgrades = listOf(
-                upgrade(id = 0L, price = 15.0),
-                upgrade(id = 1L, price = 10.0),
-                upgrade(id = 2L, price = 25.0),
-                upgrade(id = 3L, price = 30.0),
-            )
-        )
-        setInitialResourceValue(value = 1000.0)
-
         imperativeShell.updateState(
             newState = mainFunctionalCoreState(
                 balanceConfig = balanceConfig(
@@ -305,15 +242,6 @@ internal class MainViewModelTest {
 
     @Test
     fun `should update suspicion ratio names correctly`() = runBlockingTest {
-        actionsStorage = ActionsStorage(
-            initialActions = listOf(
-                action(id = 0L, ratioChanges = mapOf(RatioKey.Suspicion to 0.15f)),
-                action(id = 1L, ratioChanges = mapOf(RatioKey.Suspicion to 0.10f)),
-                action(id = 2L, ratioChanges = mapOf(RatioKey.Suspicion to 0.25f)),
-                action(id = 3L, ratioChanges = mapOf(RatioKey.Suspicion to 0.30f)),
-            )
-        )
-
         imperativeShell.updateState(
             newState = mainFunctionalCoreState(
                 ratios = listOf(
@@ -348,13 +276,6 @@ internal class MainViewModelTest {
 
     @Test
     fun `should update both ratios correctly when actions change them both`() = runBlockingTest {
-        actionsStorage = ActionsStorage(
-            initialActions = listOf(
-                action(id = 0L, ratioChanges = mapOf(RatioKey.Suspicion to 0.15f)),
-                action(id = 1L, ratioChanges = mapOf(RatioKey.Mutanity to 0.25f)),
-            )
-        )
-
         imperativeShell.updateState(
             newState = mainFunctionalCoreState(
                 ratios = listOf(
@@ -385,16 +306,6 @@ internal class MainViewModelTest {
 
     @Test
     fun `should update blood when action clicked`() = runBlockingTest {
-        setInitialResourceValue(value = 1000.0)
-        actionsStorage = ActionsStorage(
-            initialActions = listOf(
-                action(
-                    id = 1L,
-                    resourceChanges = mapOf(ResourceKey.Blood to 50.0)
-                )
-            )
-        )
-
         imperativeShell.updateState(
             newState = mainFunctionalCoreState(
                 actions = listOf(
@@ -420,20 +331,6 @@ internal class MainViewModelTest {
     @Test
     fun `should update both resources when action with several changes clicked`() =
         runBlockingTest {
-            setInitialResourceValue(key = ResourceKey.Blood, value = 1000.0)
-            setInitialResourceValue(key = ResourceKey.Money, value = 500.0)
-            actionsStorage = ActionsStorage(
-                initialActions = listOf(
-                    action(
-                        id = 1L,
-                        resourceChanges = mapOf(
-                            ResourceKey.Blood to 50.0,
-                            ResourceKey.Money to -30.0,
-                        ),
-                    )
-                )
-            )
-
             imperativeShell.updateState(
                 newState = mainFunctionalCoreState(
                     actions = listOf(
@@ -492,16 +389,6 @@ internal class MainViewModelTest {
 
     @Test
     fun `should not apply action if it requires unavailable resources`() = runBlockingTest {
-        actionsStorage = ActionsStorage(
-            initialActions = listOf(
-                action(
-                    id = 1L,
-                    resourceChanges = mapOf(ResourceKey.Money to -30.0),
-                )
-            )
-        )
-        setInitialResourceValue(key = ResourceKey.Money, value = 10.0)
-
         imperativeShell.updateState(
             newState = mainFunctionalCoreState(
                 actions = listOf(
@@ -528,20 +415,6 @@ internal class MainViewModelTest {
     @Test
     fun `should not apply action if it requires both available and not available resources`() =
         runBlockingTest {
-            actionsStorage = ActionsStorage(
-                initialActions = listOf(
-                    action(
-                        id = 1L,
-                        resourceChanges = mapOf(
-                            ResourceKey.Money to -30.0,
-                            ResourceKey.Blood to -50.0,
-                        ),
-                    )
-                )
-            )
-            setInitialResourceValue(key = ResourceKey.Money, value = 40.0)
-            setInitialResourceValue(key = ResourceKey.Blood, value = 30.0)
-
             imperativeShell.updateState(
                 newState = mainFunctionalCoreState(
                     actions = listOf(
@@ -573,26 +446,6 @@ internal class MainViewModelTest {
 
     @Test
     fun `should disable actions if it requires not available resources`() = runBlockingTest {
-        actionsStorage = ActionsStorage(
-            initialActions = listOf(
-                action(
-                    id = 1L,
-                    resourceChanges = mapOf(
-                        ResourceKey.Money to -30.0,
-                    ),
-                    actionType = ActionType.Human,
-                ),
-                action(
-                    id = 2L,
-                    resourceChanges = mapOf(
-                        ResourceKey.Money to -50.0,
-                    ),
-                    actionType = ActionType.Human,
-                )
-            )
-        )
-        setInitialResourceValue(key = ResourceKey.Money, value = 35.0)
-
         imperativeShell.updateState(
             newState = mainFunctionalCoreState(
                 actions = listOf(
@@ -630,33 +483,6 @@ internal class MainViewModelTest {
 
     @Test
     fun `show enabled actions before disabled if got both`() = runBlockingTest {
-        actionsStorage = ActionsStorage(
-            initialActions = listOf(
-                action(
-                    id = 1L,
-                    resourceChanges = mapOf(
-                        ResourceKey.Money to -60.0,
-                    ),
-                    actionType = ActionType.Human,
-                ),
-                action(
-                    id = 2L,
-                    resourceChanges = mapOf(
-                        ResourceKey.Money to -10.0,
-                    ),
-                    actionType = ActionType.Human,
-                ),
-                action(
-                    id = 3L,
-                    resourceChanges = mapOf(
-                        ResourceKey.Money to -50.0,
-                    ),
-                    actionType = ActionType.Human,
-                ),
-            )
-        )
-        setInitialResourceValue(key = ResourceKey.Money, value = 35.0)
-
         imperativeShell.updateState(
             newState = mainFunctionalCoreState(
                 actions = listOf(
@@ -703,7 +529,7 @@ internal class MainViewModelTest {
             Undertaker,
             Butcher,
         )
-        debugConfigStorage.updateAvailableJobs(availableJobs)
+
         imperativeShell.updateState(
             newState = mainFunctionalCoreState(
                 availableJobs = availableJobs,
@@ -793,7 +619,6 @@ internal class MainViewModelTest {
                 SocialJob,
             )
         )
-        playerStorage.update(newPlayer = player(tags = playerTags))
         imperativeShell.updateState(
             newState = mainFunctionalCoreState(
                 player = player(
