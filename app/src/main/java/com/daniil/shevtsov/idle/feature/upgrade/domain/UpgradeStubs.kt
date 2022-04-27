ca61542576284c6d633b44438a32097abd388f06
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-04-27 10:57:41 +0300

    Create tag field of upgrade

--- app/src/main/java/com/daniil/shevtsov/idle/feature/upgrade/domain/UpgradeStubs.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/feature/upgrade/domain/UpgradeStubs.kt
@@ -1,17 +1,22 @@
 package com.daniil.shevtsov.idle.feature.upgrade.domain
 
+import com.daniil.shevtsov.idle.feature.tagsystem.domain.Tag
+import com.daniil.shevtsov.idle.feature.tagsystem.domain.TagRelation
+
 fun upgrade(
     id: Long = 0L,
     title: String = "",
     subtitle: String = "",
     price: Double = 0.0,
     status: UpgradeStatus = UpgradeStatus.NotBought,
+    tags: Map<Tag, TagRelation> = mapOf(),
 ) = Upgrade(
     id = id,
     title = title,
     subtitle = subtitle,
     price = price(value = price),
     status = status,
+    tags = tags,
 )
 
 fun price(
