4b37e3f1f731de2d1ab8075e160c1c889d1837da
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-05-04 12:39:28 +0300

    Add icon to species view

--- app/src/main/java/com/daniil/shevtsov/idle/feature/gamestart/view/SpeciesSelection.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/feature/gamestart/view/SpeciesSelection.kt
@@ -3,15 +3,20 @@ package com.daniil.shevtsov.idle.feature.gamestart.view
 import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.SpaceBetween
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.daniil.shevtsov.idle.core.ui.Icons
 import com.daniil.shevtsov.idle.core.ui.Pallete
 import com.daniil.shevtsov.idle.core.ui.cavitary
 import com.daniil.shevtsov.idle.feature.gamestart.presentation.SpeciesSelectionItem
@@ -47,30 +52,67 @@ fun SpeciesSelection(
         ) {
             selection.forEach { speciesItem ->
                 Column(
-                    modifier = modifier
-                        .background(Pallete.LightRed)
-                        .padding(start = 1.dp, top = 1.dp)
-                        .background(Pallete.Red)
-                        .width(200.dp)
+                    horizontalAlignment = CenterHorizontally,
                 ) {
-                    Text(
-                        modifier = modifier.fillMaxWidth(),
-                        text = speciesItem.title,
-                        textAlign = TextAlign.Center,
-                        color = Color.White,
-                    )
-                    Text(
+                    Box(
                         modifier = modifier
-                            .fillMaxHeight()
-                            .padding(4.dp)
-                            .cavitary(
-                                lightColor = Pallete.LightRed,
-                                darkColor = Pallete.DarkRed
-                            )
-                            .background(Color.White)
-                            .padding(4.dp),
-                        text = speciesItem.description,
-                    )
+                            .size(35.dp)
+                            .background(Pallete.LightRed)
+                            .padding(start = 1.dp, top = 1.dp, end = 1.dp)
+                            .background(Pallete.Red),
+                        contentAlignment = Center
+                    ) {
+                        Text(text = speciesItem.icon, fontSize = 24.sp)
+                    }
+                    Row(
+                        modifier = modifier
+                            .height(1.dp)
+                            .width(200.dp)
+                            .background(Pallete.LightRed),
+                        horizontalArrangement = SpaceBetween
+                    ) {
+                        Box(
+                            modifier = modifier
+                                .height(1.dp)
+                        )
+                        Box(
+                            modifier = modifier
+                                .height(1.dp)
+                                .width(35.dp)
+                                .padding(horizontal = 1.dp)
+                                .background(Pallete.Red)
+                        )
+                        Box(
+                            modifier = modifier
+                                .height(1.dp)
+                        )
+                    }
+                    Column(
+                        modifier = modifier
+                            .background(Pallete.LightRed)
+                            .padding(start = 1.dp)
+                            .background(Pallete.Red)
+                            .width(200.dp)
+                    ) {
+                        Text(
+                            modifier = modifier.fillMaxWidth(),
+                            text = speciesItem.title,
+                            textAlign = TextAlign.Center,
+                            color = Color.White,
+                        )
+                        Text(
+                            modifier = modifier
+                                .fillMaxHeight()
+                                .padding(4.dp)
+                                .cavitary(
+                                    lightColor = Pallete.LightRed,
+                                    darkColor = Pallete.DarkRed
+                                )
+                                .background(Color.White)
+                                .padding(4.dp),
+                            text = speciesItem.description,
+                        )
+                    }
                 }
             }
         }
@@ -80,10 +122,12 @@ fun SpeciesSelection(
 fun speciesSelectionComposeStub() = listOf(
     speciesSelectionItem(
         title = "Devourer",
+        icon = Icons.Devourer,
         description = "You are a growing creature with insatiable hunger",
     ),
     speciesSelectionItem(
         title = "Vampire",
+        icon = Icons.Vampire,
         description = "You are a bloodsucking immortal creature",
     ),
 )
