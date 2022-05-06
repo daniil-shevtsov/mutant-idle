d194a743256c774ebeed4a278e937d5ebb01eafb
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-05-06 10:36:35 +0300

    Tring to fix width

--- app/src/main/java/com/daniil/shevtsov/idle/feature/gamestart/view/SpeciesSelection.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/feature/gamestart/view/SpeciesSelection.kt
@@ -2,7 +2,9 @@ package com.daniil.shevtsov.idle.feature.gamestart.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,7 +22,7 @@ import com.daniil.shevtsov.idle.feature.gamestart.presentation.SpeciesSelectionI
 import com.daniil.shevtsov.idle.feature.gamestart.presentation.speciesSelectionItem
 
 @Preview(
-    widthDp = 650,
+    widthDp = 450,
     heightDp = 600
 )
 @Composable
@@ -41,148 +43,161 @@ fun SpeciesSelection(
     onItemClicked: (id: Long) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally),
+    Box(
         modifier = modifier
             .background(Pallete.Red)
-            .padding(bottom = 4.dp)
+            .padding(4.dp)
+            .cavitary(
+                lightColor = Pallete.LightRed,
+                darkColor = Pallete.DarkRed
+            )
             .background(Pallete.DarkGray)
-            .padding(top = 4.dp)
-            .height(IntrinsicSize.Max),
+            .padding(4.dp)
+            .fillMaxWidth()
+            .horizontalScroll(rememberScrollState())
     ) {
-        selection.forEachIndexed { index, item ->
-            Column(
-                horizontalAlignment = CenterHorizontally,
-                modifier = if (item.isSelected) {
-                    modifier
-                        .weight(1f)
-                } else {
-                    modifier
-                        .padding(bottom = 2.dp)
-                        .weight(1f)
-                }
-                    .clickable { onItemClicked(item.id) }
-            ) {
-                Box(
-                    modifier = modifier
-                        .size(35.dp)
-                        .let { modifier ->
-                            if (item.isSelected) {
-                                modifier
-                                    .background(Pallete.LightRed)
-                                    .padding(start = 1.dp, top = 1.dp, end = 1.dp)
-                            } else {
-                                modifier
-                            }
-                        }
-
-                        .background(Pallete.Red),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text(text = item.icon, fontSize = 24.sp)
-                }
-                Row(
-                    modifier = modifier
-                        .height(1.dp)
-                        .fillMaxWidth()
-                        .let { modifier ->
-                            if (item.isSelected) {
-                                modifier.background(Pallete.LightRed)
-                            } else {
-                                modifier
-                            }
-                        },
-                    horizontalArrangement = Arrangement.Center
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally),
+            modifier = modifier
+                .background(Pallete.Red)
+                .padding(bottom = 4.dp)
+                .background(Pallete.DarkGray)
+                .padding(top = 4.dp)
+                .height(IntrinsicSize.Max)
+                .fillMaxWidth(),
+        ) {
+            selection.forEachIndexed { index, item ->
+                Column(
+                    horizontalAlignment = CenterHorizontally,
+                    modifier = if (item.isSelected) {
+                        modifier
+                    } else {
+                        modifier
+                            .padding(bottom = 2.dp)
+                    }
+                        .clickable { onItemClicked(item.id) }
                 ) {
                     Box(
                         modifier = modifier
-                            .weight(1f)
-                            .height(1.dp)
+                            .size(35.dp)
                             .let { modifier ->
                                 if (item.isSelected) {
                                     modifier
+                                        .background(Pallete.LightRed)
+                                        .padding(start = 1.dp, top = 1.dp, end = 1.dp)
                                 } else {
                                     modifier
-                                        .background(Pallete.Red)
                                 }
                             }
-                    )
-                    Box(
+
+                            .background(Pallete.Red),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = item.icon, fontSize = 24.sp)
+                    }
+                    Row(
                         modifier = modifier
-                            .width(35.dp)
                             .height(1.dp)
+                            .fillMaxWidth()
                             .let { modifier ->
                                 if (item.isSelected) {
-                                    modifier.padding(horizontal = 1.dp)
+                                    modifier.background(Pallete.LightRed)
+                                } else {
+                                    modifier
+                                }
+                            },
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Box(
+                            modifier = modifier
+                                .weight(1f)
+                                .height(1.dp)
+                                .let { modifier ->
+                                    if (item.isSelected) {
+                                        modifier
+                                    } else {
+                                        modifier
+                                            .background(Pallete.Red)
+                                    }
+                                }
+                        )
+                        Box(
+                            modifier = modifier
+                                .width(35.dp)
+                                .height(1.dp)
+                                .let { modifier ->
+                                    if (item.isSelected) {
+                                        modifier.padding(horizontal = 1.dp)
+                                    } else {
+                                        modifier
+                                    }
+                                }
+                                .background(Pallete.Red)
+                        )
+                        Box(
+                            modifier = modifier
+                                .weight(1f)
+                                .height(1.dp)
+                                .let { modifier ->
+                                    if (item.isSelected) {
+                                        modifier
+                                    } else {
+                                        modifier
+                                            .background(Pallete.Red)
+                                    }
+                                }
+                        )
+                    }
+                    Column(
+                        modifier = modifier
+                            .let { modifier ->
+                                if (item.isSelected) {
+                                    modifier
+                                        .background(Pallete.LightRed)
+                                        .let { kek ->
+                                            when (index) {
+                                                0 -> kek.padding(end = 1.dp)
+                                                selection.size - 1 -> kek.padding(
+                                                    start = 1.dp,
+                                                )
+                                                else -> kek.padding(
+                                                    start = 1.dp,
+                                                    end = 1.dp
+                                                )
+                                            }
+                                        }
+                                        .background(Pallete.Red)
                                 } else {
                                     modifier
                                 }
                             }
                             .background(Pallete.Red)
-                    )
-                    Box(
-                        modifier = modifier
-                            .weight(1f)
-                            .height(1.dp)
-                            .let { modifier ->
-                                if (item.isSelected) {
-                                    modifier
-                                } else {
-                                    modifier
-                                        .background(Pallete.Red)
-                                }
-                            }
-                    )
-                }
-                Column(
-                    modifier = modifier
-                        .let { modifier ->
-                            if (item.isSelected) {
-                                modifier
-                                    .background(Pallete.LightRed)
-                                    .let { kek ->
-                                        when (index) {
-                                            0 -> kek.padding(end = 1.dp)
-                                            selection.size - 1 -> kek.padding(
-                                                start = 1.dp,
-                                            )
-                                            else -> kek.padding(
-                                                start = 1.dp,
-                                                end = 1.dp
-                                            )
-                                        }
-                                    }
-                                    .background(Pallete.Red)
-                            } else {
-                                modifier
-                            }
-                        }
-                        .background(Pallete.Red)
-                        .padding(4.dp),
-                ) {
-                    Text(
-                        modifier = modifier.fillMaxWidth(),
-                        text = item.title,
-                        textAlign = TextAlign.Center,
-                        color = Color.White,
-                        fontSize = 18.sp,
-                    )
-                    Text(
-                        modifier = modifier
-                            .fillMaxHeight()
-                            .padding(4.dp)
-                            .cavitary(
-                                lightColor = Pallete.LightRed,
-                                darkColor = Pallete.DarkRed
-                            )
-                            .background(Color.White)
                             .padding(4.dp),
-                        text = item.description,
-                        fontSize = 14.sp,
-                    )
+                    ) {
+                        Text(
+                            modifier = modifier.fillMaxWidth(),
+                            text = item.title,
+                            textAlign = TextAlign.Center,
+                            color = Color.White,
+                            fontSize = 18.sp,
+                        )
+                        Text(
+                            modifier = modifier
+                                .fillMaxHeight()
+                                .padding(4.dp)
+                                .cavitary(
+                                    lightColor = Pallete.LightRed,
+                                    darkColor = Pallete.DarkRed
+                                )
+                                .background(Color.White)
+                                .padding(4.dp),
+                            text = item.description,
+                            fontSize = 14.sp,
+                        )
+                    }
                 }
-            }
 
+            }
         }
     }
 }
