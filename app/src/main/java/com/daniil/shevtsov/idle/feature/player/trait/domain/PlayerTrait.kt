c41fba173948a43cead6e4d61831b6dc14298d91
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-05-08 11:56:51 +0300

    [WIP]

--- app/src/main/java/com/daniil/shevtsov/idle/feature/player/trait/domain/PlayerTrait.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/feature/player/trait/domain/PlayerTrait.kt
@@ -1,6 +1,25 @@
 package com.daniil.shevtsov.idle.feature.player.trait.domain
 
+import com.daniil.shevtsov.idle.feature.tagsystem.domain.Tag
+
 data class PlayerTrait(
-    val traitId: TraitId,
     val id: Long,
+    val traitId: TraitId,
+    val title: String,
+    val description: String,
+    val tags: List<Tag>,
+)
+
+fun playerTrait(
+    id: Long = 0L,
+    traitId: TraitId = TraitId.Job,
+    title: String = "",
+    description: String = "",
+    tags: List<Tag> = emptyList(),
+) = PlayerTrait(
+    id = id,
+    traitId = traitId,
+    title = title,
+    description = description,
+    tags = tags,
 )
