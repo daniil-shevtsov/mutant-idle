7360d50b1f16c4be1bd5d956560f68d1649b20ec
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-05-08 12:43:00 +0300

    [WIP]

--- app/src/test/java/com/daniil/shevtsov/idle/feature/player/core/domain/PlayerAsserts.kt
+++ app/src/test/java/com/daniil/shevtsov/idle/feature/player/core/domain/PlayerAsserts.kt
@@ -2,12 +2,17 @@ package com.daniil.shevtsov.idle.feature.player.core.domain
 
 import assertk.Assert
 import assertk.assertions.isEqualTo
+import assertk.assertions.isNotNull
 import assertk.assertions.prop
 import com.daniil.shevtsov.idle.feature.player.job.domain.PlayerJob
 import com.daniil.shevtsov.idle.feature.player.species.domain.PlayerSpecies
+import com.daniil.shevtsov.idle.feature.player.trait.domain.PlayerTrait
+import com.daniil.shevtsov.idle.feature.player.trait.domain.TraitId
 
-fun Assert<Player>.assertSpeciesSelected(id: Long) = prop(Player::species)
-    .prop(PlayerSpecies::id)
+fun Assert<Player>.assertSpeciesSelected(id: Long) = prop(Player::traits)
+    .prop(name = "Lol", extract = { traits -> traits[TraitId.Species] })
+    .isNotNull()
+    .prop(PlayerTrait::id)
     .isEqualTo(id)
 
 fun Assert<Player>.assertJobSelected(id: Long) = prop(Player::job)
