e1e53f32960e8a25ba015e853e1dbd42da070ca0
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-05-08 12:40:11 +0300

    [WIP]

--- app/src/test/java/com/daniil/shevtsov/idle/feature/player/core/domain/PlayerAsserts.kt
+++ app/src/test/java/com/daniil/shevtsov/idle/feature/player/core/domain/PlayerAsserts.kt
@@ -3,8 +3,13 @@ package com.daniil.shevtsov.idle.feature.player.core.domain
 import assertk.Assert
 import assertk.assertions.isEqualTo
 import assertk.assertions.prop
+import com.daniil.shevtsov.idle.feature.player.job.domain.PlayerJob
 import com.daniil.shevtsov.idle.feature.player.species.domain.PlayerSpecies
 
 fun Assert<Player>.assertSpeciesSelected(id: Long) = prop(Player::species)
     .prop(PlayerSpecies::id)
+    .isEqualTo(id)
+
+fun Assert<Player>.assertJobSelected(id: Long) = prop(Player::job)
+    .prop(PlayerJob::id)
     .isEqualTo(id)
