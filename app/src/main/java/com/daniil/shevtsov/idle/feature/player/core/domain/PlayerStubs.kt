fc17f5e2d88504763d27de81382f7951e4560146
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-05-08 12:02:46 +0300

    [WIP]

--- app/src/main/java/com/daniil/shevtsov/idle/feature/player/core/domain/PlayerStubs.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/feature/player/core/domain/PlayerStubs.kt
@@ -4,12 +4,15 @@ import com.daniil.shevtsov.idle.feature.player.job.domain.PlayerJob
 import com.daniil.shevtsov.idle.feature.player.job.domain.playerJob
 import com.daniil.shevtsov.idle.feature.player.species.domain.PlayerSpecies
 import com.daniil.shevtsov.idle.feature.player.species.domain.playerSpecies
+import com.daniil.shevtsov.idle.feature.player.trait.domain.PlayerTrait
+import com.daniil.shevtsov.idle.feature.player.trait.domain.TraitId
 import com.daniil.shevtsov.idle.feature.tagsystem.domain.Tag
 
 fun player(
     job: PlayerJob = playerJob(),
     species: PlayerSpecies = playerSpecies(),
     generalTags: List<Tag> = emptyList(),
+    traits: Map<TraitId, PlayerTrait> = mapOf(),
 ) = Player(
     job = job,
     species = species,
--- app/src/test/java/com/daniil/shevtsov/idle/feature/player/core/domain/PlayerFunctionalCoreTest.kt
+++ app/src/test/java/com/daniil/shevtsov/idle/feature/player/core/domain/PlayerFunctionalCoreTest.kt
@@ -2,6 +2,7 @@ package com.daniil.shevtsov.idle.feature.player.core.domain
 
 import com.daniil.shevtsov.idle.feature.coreshell.domain.gameState
 import com.daniil.shevtsov.idle.feature.player.trait.domain.TraitId
+import com.daniil.shevtsov.idle.feature.player.trait.domain.playerTrait
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
@@ -9,10 +10,17 @@ internal class PlayerFunctionalCoreTest {
 
     @Test
     fun `should update player traits when trait selected`() {
-        val initialState = gameState()
+        val initialPlayer = player(
+            traits = mapOf(
+                TraitId.Job to playerTrait(id = 1L, title = "Initial Job")
+            )
+        )
+        val initialState = gameState(
+            player = initialPlayer
+        )
         val newState = playerFunctionalCore(
             state = initialState,
-            action = PlayerViewAction.ChangeTrait(traitId = TraitId.Job, id = 1L),
+            action = PlayerViewAction.ChangeTrait(traitId = TraitId.Job, id = 2L),
         )
     }
 
