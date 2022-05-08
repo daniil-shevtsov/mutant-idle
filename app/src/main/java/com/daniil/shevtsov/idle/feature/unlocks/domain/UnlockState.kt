721d93153f1327de40bde7a70020b21afa913e0a
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-05-08 20:10:42 +0300

    [WIP]

--- app/src/main/java/com/daniil/shevtsov/idle/feature/unlocks/domain/UnlockState.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/feature/unlocks/domain/UnlockState.kt
@@ -3,10 +3,17 @@ package com.daniil.shevtsov.idle.feature.unlocks.domain
 import com.daniil.shevtsov.idle.feature.player.trait.domain.TraitId
 
 data class UnlockState(
-    val species: Map<Long, Boolean>,
+    var species: Map<Long, Boolean>,
     val jobs: Map<Long, Boolean>,
     val traits: Map<TraitId, Map<Long, Boolean>>,
-)
+) {
+    init {
+        species = when (traits.isNotEmpty()) {
+            true -> traits[TraitId.Species]!!
+            false -> species
+        }
+    }
+}
 
 fun unlockState(
     species: Map<Long, Boolean> = mapOf(),
