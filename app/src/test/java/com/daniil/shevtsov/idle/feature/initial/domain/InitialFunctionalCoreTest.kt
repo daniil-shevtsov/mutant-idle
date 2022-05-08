ff2225be7ff209bc2c8d7d5787a698ef391f9687
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-05-08 14:57:03 +0300

    [WIP]

---
+++ app/src/test/java/com/daniil/shevtsov/idle/feature/initial/domain/InitialFunctionalCoreTest.kt
@@ -0,0 +1,9 @@
+package com.daniil.shevtsov.idle.feature.initial.domain
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class InitialFunctionalCoreTest {
+
+
+}
