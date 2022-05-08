b074282bbfdec2e6b72d5343068b77d09315dd56
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-05-08 11:51:57 +0300

    [WIP]

---
+++ app/src/main/java/com/daniil/shevtsov/idle/feature/player/core/domain/PlayerFunctionalCore.kt
@@ -0,0 +1,11 @@
+package com.daniil.shevtsov.idle.feature.player.core.domain
+
+import com.daniil.shevtsov.idle.feature.coreshell.domain.GameState
+
+fun playerFunctionalCore(
+    state: GameState,
+    action: PlayerViewAction,
+): GameState {
+
+    return state
+}
---
+++ app/src/main/java/com/daniil/shevtsov/idle/feature/player/core/domain/PlayerViewAction.kt
@@ -0,0 +1,4 @@
+package com.daniil.shevtsov.idle.feature.player.core.domain
+
+sealed class PlayerViewAction {
+}
