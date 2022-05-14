21fc97caff01f7000c2b5759ef6b582aa08b26a7
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-05-14 15:26:59 +0300

    Start adding colors

--- app/src/main/java/com/daniil/shevtsov/idle/core/ui/theme/AppColors.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/core/ui/theme/AppColors.kt
@@ -65,50 +65,6 @@ class AppColors(
     }
 }
 
-fun devourerColors(): AppColors = AppColors(
-    backgroundLight = Color(0xFFD64747),
-    background = Color(0xFFAF0A0A),
-    backgroundDark = Color(0xFF750404),
-    backgroundDarkest = Color(0xFF2F1B1B),
-    backgroundText = Color.White,
-    textDark = Color.Black,
-    textLight = Color.White,
-    iconLight = Color.White,
-)
-
-fun alienColors(): AppColors = AppColors(
-    backgroundLight = Color(0xFFCDDEE5),
-    background = Color(0xFF94ACB6),
-    backgroundDark = Color(0xFF5A6E76),
-    backgroundDarkest = Color(0xFF36454B),
-    backgroundText = Color.White,
-    textDark = Color(0xFF26235A),
-    textLight = Color(0xFFE0DEFB),
-    iconLight = Color(0xFFE0DEFB),
-)
-
-fun androidColors(): AppColors = AppColors(
-    backgroundLight = Color(0xFFCDDEE5),
-    background = Color(0xFF94ACB6),
-    backgroundDark = Color(0xFF5A6E76),
-    backgroundDarkest = Color(0xFF36454B),
-    backgroundText = Color.White,
-    textDark = Color(0xFF26235A),
-    textLight = Color(0xFFE0DEFB),
-    iconLight = Color(0xFFE0DEFB),
-)
-
-fun vampireColors(): AppColors = AppColors(
-    backgroundLight = Color(0xFFAF3550),
-    background = Color(0xFF911A34),
-    backgroundDark = Color(0xFF600B1E),
-    backgroundDarkest = Color(0xFF2F000F),
-    backgroundText = Color(0xFFFACBE2),
-    textDark = Color(0xFFc79ab0),
-    textLight = Color(0xFFfffeff),
-    iconLight = Color(0xFFfffeff),
-)
-
 fun testColors(): AppColors = AppColors(
     backgroundLight = Color(0xFFFF0000),
     background = Color(0xFFFF0000),
--- app/src/main/java/com/daniil/shevtsov/idle/core/ui/theme/SpeciesColors.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/core/ui/theme/SpeciesColors.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.daniil.shevtsov.idle.core.ui.upgradePreviewStub
@@ -45,3 +46,47 @@ fun chooseColorsForId(speciesId: Long): AppColors {
         else -> devourerColors()
     }
 }
+
+fun devourerColors(): AppColors = AppColors(
+    backgroundLight = Color(0xFFD64747),
+    background = Color(0xFFAF0A0A),
+    backgroundDark = Color(0xFF750404),
+    backgroundDarkest = Color(0xFF2F1B1B),
+    backgroundText = Color.White,
+    textDark = Color.Black,
+    textLight = Color.White,
+    iconLight = Color.White,
+)
+
+fun alienColors(): AppColors = AppColors(
+    backgroundLight = Color(0xFFa0d29e),
+    background = Color(0xFF4F9766),
+    backgroundDark = Color(0xFF487463),
+    backgroundDarkest = Color(0xFF225149),
+    backgroundText = Color(0xFFE0FFF1),
+    textDark = Color(0xFF212B27),
+    textLight = Color(0xFFDFFCF1),
+    iconLight = Color(0xFFDFFCF1),
+)
+
+fun androidColors(): AppColors = AppColors(
+    backgroundLight = Color(0xFFCDDEE5),
+    background = Color(0xFF94ACB6),
+    backgroundDark = Color(0xFF5A6E76),
+    backgroundDarkest = Color(0xFF36454B),
+    backgroundText = Color.White,
+    textDark = Color(0xFF26235A),
+    textLight = Color(0xFFE0DEFB),
+    iconLight = Color(0xFFE0DEFB),
+)
+
+fun vampireColors(): AppColors = AppColors(
+    backgroundLight = Color(0xFFAF3550),
+    background = Color(0xFF911A34),
+    backgroundDark = Color(0xFF600B1E),
+    backgroundDarkest = Color(0xFF2F000F),
+    backgroundText = Color(0xFFFACBE2),
+    textDark = Color(0xFFc79ab0),
+    textLight = Color(0xFFfffeff),
+    iconLight = Color(0xFFfffeff),
+)
