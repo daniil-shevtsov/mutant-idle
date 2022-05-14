7395e747aff828eafe56e84bba485265fc4b230f
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-05-14 12:11:06 +0300

    [WIP]

--- app/src/main/java/com/daniil/shevtsov/idle/feature/player/core/domain/Player.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/feature/player/core/domain/Player.kt
@@ -3,6 +3,8 @@ package com.daniil.shevtsov.idle.feature.player.core.domain
 
 import com.daniil.shevtsov.idle.feature.player.trait.domain.PlayerTrait
 import com.daniil.shevtsov.idle.feature.player.trait.domain.TraitId
+import com.daniil.shevtsov.idle.feature.ratio.domain.RatioKey
+import com.daniil.shevtsov.idle.feature.resource.domain.ResourceKey
 import com.daniil.shevtsov.idle.feature.tagsystem.domain.Tag
 
 data class Player(
@@ -11,4 +13,9 @@ data class Player(
 ) {
     val tags: List<Tag>
         get() = generalTags + traits.values.map { it.tags }.flatten()
+
+    val mainResourceKey: ResourceKey
+        get() = traits[TraitId.Species]?.mainResource ?: ResourceKey.Blood
+    val mainRatioKey: RatioKey
+        get() = traits[TraitId.Species]?.mainRatio ?: RatioKey.Mutanity
 }
