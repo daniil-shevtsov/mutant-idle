7acd4f2d0127c264758ce3cf548b7e684614e795
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-05-14 12:40:43 +0300

    [WIP]

--- app/src/main/java/com/daniil/shevtsov/idle/feature/main/domain/MainFunctionalCore.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/feature/main/domain/MainFunctionalCore.kt
@@ -1,10 +1,10 @@
 package com.daniil.shevtsov.idle.feature.main.domain
 
 import com.daniil.shevtsov.idle.core.navigation.Screen
-import com.daniil.shevtsov.idle.feature.action.domain.Action
 import com.daniil.shevtsov.idle.feature.coreshell.domain.GameState
 import com.daniil.shevtsov.idle.feature.drawer.presentation.DrawerViewAction
 import com.daniil.shevtsov.idle.feature.main.presentation.MainViewAction
+import com.daniil.shevtsov.idle.feature.ratio.domain.Ratio
 import com.daniil.shevtsov.idle.feature.ratio.domain.RatioKey
 import com.daniil.shevtsov.idle.feature.resource.domain.Resource
 import com.daniil.shevtsov.idle.feature.resource.domain.ResourceKey
@@ -117,7 +117,7 @@ fun handleActionClicked(
         resourceChanges = selectedAction.resourceChanges
     )
 
-    val updatedRatios = applyRatioChanges(state, selectedAction)
+    val updatedRatios = applyRatioChanges(state, selectedAction.ratioChanges)
 
     val newTags =
         state.player.generalTags + selectedAction.tags[TagRelation.Provides].orEmpty() - selectedAction.tags[TagRelation.Removes].orEmpty()
@@ -141,9 +141,9 @@ fun handleActionClicked(
 
 private fun applyRatioChanges(
     state: GameState,
-    selectedAction: Action
-) = state.ratios.map { ratio ->
-    when (val ratioChange = selectedAction.ratioChanges[ratio.key]) {
+    ratioChanges: Map<RatioKey, Float>
+): List<Ratio> = state.ratios.map { ratio ->
+    when (val ratioChange = ratioChanges[ratio.key]) {
         null -> ratio
         else -> ratio.copy(value = ratio.value + ratioChange)
     }
