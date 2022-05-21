bb8a1a83ce712edbff53664694ccaf2c3f129546
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-05-21 11:08:30 +0300

    Create random points

--- app/src/main/java/com/daniil/shevtsov/idle/core/ui/shapes/Shapes.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/core/ui/shapes/Shapes.kt
@@ -16,13 +16,24 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlin.random.Random
 
-private fun Path.drawMyPath(size: Size) {
-    val h = size.height
-    val halfH = h / 2f
-    val w = size.width
-    val delta = size.width / 3f
+private fun createSegments(
+    size: Size,
+    numberOfSegments: Int = 10,
+    randomFloats: List<Float>,
+    delta: Float = size.height * 0.1f,
+) = randomFloats.mapIndexed { segment, randomFloat ->
+    Offset(
+        x = (size.width / numberOfSegments) * segment,
+        y = 30 - (delta - randomFloat * delta)
+    )
+}
 
+private fun Path.drawMyPath(
+    size: Size,
+    segments: List<Offset>,
+) {
     val topLeft = Offset(0f, 0f)
     val topRight = Offset(size.width, 0f)
     val bottomRight = Offset(size.width, size.height)
@@ -39,6 +50,9 @@ private fun Path.drawMyPath(size: Size) {
 @Composable
 @Preview
 fun Shape() {
+    val numberOfSegments = 10
+    val randomFloats = IntRange(0, numberOfSegments).map { Random.nextFloat() }
+
     val shape = GenericShape { size, _ ->
 
         val topLeft = Offset(0f, 0f)
@@ -47,7 +61,10 @@ fun Shape() {
         val bottomLeft = Offset(0f, size.height)
         val center = topLeft.plus(Offset(size.width, size.height))
 
-        drawMyPath(size)
+        drawMyPath(
+            size,
+            createSegments(size, numberOfSegments, randomFloats)
+        )
     }
     Box(
         modifier = Modifier
@@ -82,13 +99,19 @@ fun Shape() {
                 val bottomLeft = Offset(0f, size.height)
                 val center = topLeft.plus(Offset(size.width / 2, size.height / 2))
 
-                drawPath(Path().apply { drawMyPath(size) }, Color.Cyan)
+                val segments = createSegments(size, numberOfSegments, randomFloats)
+
+                drawPath(Path().apply { drawMyPath(size, segments) }, Color.Cyan)
 
                 addOvalAt(topLeft, color = Color.Black)
                 addOvalAt(topRight, color = Color.Blue)
                 addOvalAt(bottomRight, color = Color.Green)
                 addOvalAt(bottomLeft, color = Color.Gray)
                 addOvalAt(center)
+
+                segments.forEach { segment ->
+                    addOvalAt(segment, radius = 5f, color = Color.Yellow)
+                }
             }
         )
     }
