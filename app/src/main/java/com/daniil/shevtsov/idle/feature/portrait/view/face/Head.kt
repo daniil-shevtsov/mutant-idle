e2dbb5cc2ac9a90d30f21d5f7f02036fd2cf64f9
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-05-29 22:34:36 +0300

    Fix first bezier translation

--- app/src/main/java/com/daniil/shevtsov/idle/feature/portrait/view/face/Head.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/feature/portrait/view/face/Head.kt
@@ -51,12 +51,15 @@ fun DrawScope.drawHead(headArea: Rect) {
         color = Color.Gray
     )
 
+    val supportY = ((topHeadArea.bottomLeft.y - topHeadArea.topLeft.y) * 1.0f) * 2
+    val supportX = ((topHeadArea.bottomCenter.x - topHeadArea.bottomLeft.x) * 1.0f) * 2
+
     clipPath(path = Path().apply {
         val state = BezierState(
             start = topHeadArea.bottomLeft,
             finish = topHeadArea.bottomRight,
-            support = topHeadArea.topLeft,
-            support2 = topHeadArea.topRight,
+            support = topHeadArea.bottomLeft.translate(x = -supportX, y = -supportY),
+            support2 = topHeadArea.bottomRight.translate(x = supportX, y = -supportY),
         )
         drawQuadraticBezier(state)
         lineTo(bottomArea.bottomRight.x, bottomArea.bottomRight.y)
@@ -65,6 +68,7 @@ fun DrawScope.drawHead(headArea: Rect) {
         close()
     }, clipOp = ClipOp.Intersect) {
         drawBodyPart(head)
+        drawBodyPart(BodyPart(topHeadArea.topLeft, topHeadArea.size, Color.Green))
     }
 
 
