6ac0c9682629fa7a24fe6fb0b722f419e2d42c68
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-06-05 11:34:40 +0300

    Start creating editor of bezier

--- app/src/main/java/com/daniil/shevtsov/idle/feature/portrait/view/face/Head.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/feature/portrait/view/face/Head.kt
@@ -1,8 +1,11 @@
 package com.daniil.shevtsov.idle.feature.portrait.view.face
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
@@ -15,16 +18,24 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.daniil.shevtsov.idle.feature.portrait.view.*
 
-@Preview
+@Preview(
+    widthDp = 800,
+    heightDp = 800,
+)
 @Composable
 fun HeadPreview() {
-    Canvas(modifier = Modifier.size(400.dp), onDraw = {
-        val headArea = Rect(
-            offset = center.translate(-size.height / 2f),
-            size = size,
-        )
-        drawHead(headArea = headArea)
-    })
+    Box(
+        modifier = Modifier.size(800.dp).background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Canvas(modifier = Modifier.size(400.dp), onDraw = {
+            val headArea = Rect(
+                offset = center.translate(-size.height / 2f),
+                size = size,
+            )
+            drawHead(headArea = headArea)
+        })
+    }
 }
 
 fun DrawScope.drawHead(headArea: Rect) {
@@ -53,14 +64,14 @@ fun DrawScope.drawHead(headArea: Rect) {
 
     val supportY = ((topHeadArea.bottomLeft.y - topHeadArea.topLeft.y) * 0.5f) * 2
     val supportX = ((topHeadArea.bottomCenter.x - topHeadArea.bottomLeft.x) * 0.5f) * 2
-
+    val state = BezierState(
+        topHeadArea.bottomLeft,
+        topHeadArea.bottomRight,
+        topHeadArea.bottomCenter.translate(x = -topHeadArea.width, y = -topHeadArea.height),
+        topHeadArea.bottomCenter.translate(x = topHeadArea.width, y = -topHeadArea.height),
+    )
     clipPath(path = Path().apply {
-        val state = BezierState(
-            topHeadArea.bottomLeft,
-            topHeadArea.bottomRight,
-            topHeadArea.bottomCenter.translate(x = -supportX, y = -supportY),
-            topHeadArea.bottomCenter.translate(x = supportX, y = -supportY),
-        )
+
         drawQuadraticBezier(state)
         lineTo(bottomArea.bottomRight.x, bottomArea.bottomRight.y)
         lineTo(bottomArea.bottomLeft.x, bottomArea.bottomLeft.y)
@@ -68,7 +79,7 @@ fun DrawScope.drawHead(headArea: Rect) {
         close()
     }, clipOp = ClipOp.Intersect) {
         drawBodyPart(head)
-        drawBodyPart(BodyPart(topHeadArea.topLeft, topHeadArea.size, Color.Green))
+        drawBodyPart(BodyPart(topHeadArea.topLeft, topHeadArea.size, Color.Blue))
     }
 
 
@@ -77,4 +88,5 @@ fun DrawScope.drawHead(headArea: Rect) {
     drawArea(middleArea)
     drawArea(bottomArea)
 
+    drawBezierPoints(state)
 }
