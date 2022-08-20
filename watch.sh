83b67718d01c1760302922ce4b81f7fe38b9e842
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-08-20 17:23:59 +0300

    [WIP]

--- watch.sh
+++ watch.sh
@@ -2,5 +2,5 @@
 while true
 do
    inotifywait.exe -r -e modify ./app/src/main/
-  ./gradlew app:test && git commit -am "[WIP]" || git checkout main -- ./app/src/main/
+  ./gradlew app:test && git commit -am "[WIP]" || git checkout HEAD -- ./app/src/main/
 done
