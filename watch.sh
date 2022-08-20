8e84e41338baf096040e4ca938572db54220c128
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-08-20 17:15:58 +0300

    kek

--- watch.sh
+++ watch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 while true
 do
-   inotifywait.exe -r -e modify .app/src
+   inotifywait.exe -r -e modify ./app/src
   ./gradlew app:test && git commit -am "[WIP]" || git reset --hard
 done
