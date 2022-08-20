d89e316fb6178476ebdce7992d8a305cc1048caf
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-08-20 17:13:18 +0300

    update script

--- watch.sh
+++ watch.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 #while true
 #do
-    #inotifywait -r -e modify .app/src
-    ./gradlew app:test && git commit -am "[WIP]" || git reset --hard
+   inotifywait.exe -r -e modify .app/src
+  ./gradlew app:test && git commit -am "[WIP]" || git reset --hard
 #done
-"./gradlew app:test && git commit -am [WIP] || git reset --hard"
