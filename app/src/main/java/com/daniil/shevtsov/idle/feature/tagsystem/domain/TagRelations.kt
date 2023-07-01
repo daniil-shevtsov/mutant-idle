6b77e8431818a31b69ef32826d433bf9b5bf53aa
Daniil Shevtsov <me@daniilshevtsov.com>
2023-07-01 14:58:51 +0400

    kek

--- app/src/main/java/com/daniil/shevtsov/idle/feature/tagsystem/domain/TagRelations.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/feature/tagsystem/domain/TagRelations.kt
@@ -2,8 +2,20 @@ package com.daniil.shevtsov.idle.feature.tagsystem.domain
 
 typealias TagRelations = Map<TagRelation, List<Tag>>
 
+@JvmName("tagRelationsWithList")
 fun tagRelations(
     vararg values: Pair<TagRelation, List<Tag>>
-): TagRelations = mapOf(*values)
+): TagRelations = createTagRelations(values.toList())
 
-fun noTagRelations(): TagRelations = tagRelations()
+fun tagRelations(
+    vararg values: Pair<TagRelation, Tag>
+): TagRelations = createTagRelations(
+    values
+        .map { (tagRelation, tag) -> tagRelation to listOf(tag) }
+)
+
+fun noTagRelations(): TagRelations = createTagRelations(emptyList())
+
+private fun createTagRelations(
+    values: List<Pair<TagRelation, List<Tag>>>
+): TagRelations = mapOf(*values.toTypedArray())
