391c9c943ee466bff1878564e8106d4fad3953bb
Daniil Shevtsov <me@daniilshevtsov.com>
2023-07-08 12:15:31 +0400

    Create switch mockup

--- app/src/main/java/com/daniil/shevtsov/idle/feature/settings/view/SettingsScreen.kt
+++ app/src/main/java/com/daniil/shevtsov/idle/feature/settings/view/SettingsScreen.kt
@@ -1,11 +1,19 @@
 package com.daniil.shevtsov.idle.feature.settings.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement.spacedBy
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.daniil.shevtsov.idle.core.ui.theme.AppTheme
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 
@@ -26,13 +34,21 @@ fun settingsViewStateComposeStub(): SettingsViewState {
         ),
         settingsPanel = SettingsPanelModel(
             items = persistentListOf(
-                SettingsItem.Switch(id = 0L, isSelected = true),
-                SettingsItem.Switch(id = 1L, isSelected = false),
-                SettingsItem.Switch(id = 2L, isSelected = true),
-                SettingsItem.ColorSelector(id = 3L, currentColor = SettingsColor("#FF0000")),
-                SettingsItem.Switch(id = 4L, isSelected = true),
-                SettingsItem.Switch(id = 5L, isSelected = false),
-                SettingsItem.ColorSelector(id = 6L, currentColor = SettingsColor("#00FF00")),
+                SettingsItem.Switch(id = 0L, title = "lol", isSelected = true),
+                SettingsItem.Switch(id = 1L, title = "kek", isSelected = false),
+                SettingsItem.Switch(id = 2L, title = "cheburek", isSelected = true),
+                SettingsItem.ColorSelector(
+                    id = 3L,
+                    title = "lol color",
+                    currentColor = SettingsColor("#FF0000")
+                ),
+                SettingsItem.Switch(id = 4L, title = "lol2", isSelected = true),
+                SettingsItem.Switch(id = 5L, title = "kek2", isSelected = false),
+                SettingsItem.ColorSelector(
+                    id = 6L,
+                    title = "cheburek color",
+                    currentColor = SettingsColor("#00FF00")
+                ),
             )
         )
     )
@@ -48,7 +64,10 @@ fun SettingsScreen(
     state: SettingsViewState,
     modifier: Modifier = Modifier
 ) {
-    Row(modifier = modifier) {
+    Row(
+        modifier = modifier
+            .background(AppTheme.colors.background),
+    ) {
         Column {
             state.categories.forEach { category ->
                 SettingsCategory(category)
@@ -84,7 +103,33 @@ fun ColorSelector(model: SettingsItem.ColorSelector) {
 
 @Composable
 fun SettingsSwitch(model: SettingsItem.Switch) {
-    Text(text = "Switch ${model.id}: ${model.isSelected}")
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = spacedBy(AppTheme.dimensions.paddingM)
+    ) {
+        Text(text = model.title)
+        Box {
+            Box(
+                modifier = Modifier
+                    .width(40.dp)
+                    .height(20.dp)
+                    .background(AppTheme.colors.backgroundDark)
+                    .align(Alignment.Center)
+            )
+            Box(
+                modifier = Modifier
+                    .width(20.dp)
+                    .height(20.dp)
+                    .background(AppTheme.colors.backgroundLight)
+                    .align(
+                        when (model.isSelected) {
+                            true -> Alignment.CenterEnd
+                            false -> Alignment.CenterStart
+                        }
+                    )
+            )
+        }
+    }
 }
 
 data class SettingsCategoryModel(
@@ -97,8 +142,19 @@ data class SettingsPanelModel(
 )
 
 sealed interface SettingsItem {
-    data class Switch(val id: Long, val isSelected: Boolean) : SettingsItem
-    data class ColorSelector(val id: Long, val currentColor: SettingsColor) : SettingsItem
+    val title: String
+
+    data class Switch(
+        val id: Long,
+        override val title: String,
+        val isSelected: Boolean,
+    ) : SettingsItem
+
+    data class ColorSelector(
+        val id: Long,
+        override val title: String,
+        val currentColor: SettingsColor,
+    ) : SettingsItem
 }
 
 data class SettingsColor(
