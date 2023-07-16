74e2bc9d95d1c5f43567505a6f688e43a01896c4
Daniil Shevtsov <me@daniilshevtsov.com>
2023-07-16 15:24:15 +0400

    Start integrating say

--- app/src/test/java/com/daniil/shevtsov/idle/feature/tagsystem/domain/FuzzyMatchSpikeTest.kt
+++ app/src/test/java/com/daniil/shevtsov/idle/feature/tagsystem/domain/FuzzyMatchSpikeTest.kt
@@ -14,19 +14,19 @@ import org.junit.jupiter.api.Test
 
 class FuzzyMatchSpikeTest {
 
-//    @Test
-//    fun `test`() {
-//        val tags = defaultTagsWithAdditional(
-//            "time_of_day" to "day",
-//            "trespassing" to "false",
-//            "appearance" to "human",
-//            "location" to "shop",
-//        )
-//
-//        val line = say(tags)
-//
-//        assertThat(line).lastPlot().isEqualTo("Hello, what can I do for you?")
-//    }
+    @Test
+    fun `test`() {
+        val tags = defaultTagsWithAdditional(
+            "time_of_day" to "day",
+            "trespassing" to "false",
+            "appearance" to "human",
+            "location" to "shop",
+        )
+
+        val line = say(tags)
+
+        assertThat(line).lastPlot().isEqualTo("Hello, what can I do for you?")
+    }
 //
 //    @Test
 //    fun `kek2`() {
@@ -568,36 +568,39 @@ class FuzzyMatchSpikeTest {
 
 }
 
-//private fun say(tags: SpikeTags): PerformResult {
-//    val lines = listOf(
-//        listOf(
-//            "trespassing" to "true",
-//            "time_of_day" to "night",
-//            "appearance" to "demon",
-//        ) to "In the name of the lord, I say be gone!",
-//        listOf(
-//            "trespassing" to "true",
-//            "time_of_day" to "night",
-//            "appearance" to "alien",
-//        ) to "This is my shop and this is my planet! Get out!",
-//        listOf(
-//            "trespassing" to "true",
-//            "time_of_day" to "night",
-//        ) to "Who is there?! I am armed, get out while you can!",
-//        listOf(
-//            "trespassing" to "true",
-//        ) to "Hey, you are not supposed to be here. Get out!",
-//        listOf("time_of_day" to "morning") to "Morning, what can I do for you?",
-//        listOf("" to "") to "Hello, what can I do for you?"
-//    )
-//
-//    val mostSuitableLine = lines
-//        .filter { (lineTags, _) ->
-//            lineTags == listOf("" to "") || lineTags.all { (key, value) -> tags[key] == value }
-//        }
-//        .maxByOrNull { (lineTags, _) ->
-//            lineTags.count { (key, value) -> tags[key] == value }
-//        }?.second.orEmpty()
-//
-//    return PerformResult(tags, listOf(mostSuitableLine))
-//}
+private fun say(tags: SpikeTags): Game {
+    val lines = listOf(
+        listOf(
+            "trespassing" to "true",
+            "time_of_day" to "night",
+            "appearance" to "demon",
+        ) to "In the name of the lord, I say be gone!",
+        listOf(
+            "trespassing" to "true",
+            "time_of_day" to "night",
+            "appearance" to "alien",
+        ) to "This is my shop and this is my planet! Get out!",
+        listOf(
+            "trespassing" to "true",
+            "time_of_day" to "night",
+        ) to "Who is there?! I am armed, get out while you can!",
+        listOf(
+            "trespassing" to "true",
+        ) to "Hey, you are not supposed to be here. Get out!",
+        listOf("time_of_day" to "morning") to "Morning, what can I do for you?",
+        listOf("" to "") to "Hello, what can I do for you?"
+    )
+
+    val mostSuitableLine = lines
+        .filter { (lineTags, _) ->
+            lineTags == listOf("" to "") || lineTags.all { (key, value) -> tags[tagKey(key)]?.value == value }
+        }
+        .maxByOrNull { (lineTags, _) ->
+            lineTags.count { (key, value) -> tags[tagKey(key)]?.value == value }
+        }?.second.orEmpty()
+
+    return game(
+        tags = tags,
+        plot = listOf(mostSuitableLine)
+    )
+}
