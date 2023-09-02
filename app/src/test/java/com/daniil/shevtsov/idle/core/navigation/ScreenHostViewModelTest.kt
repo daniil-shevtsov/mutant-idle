043dc97271d0f67bb11b03084ed64e272ea28526
Daniil Shevtsov <me@daniilshevtsov.com>
2023-09-02 12:48:22 +0400

    create first test

--- app/src/test/java/com/daniil/shevtsov/idle/core/navigation/ScreenHostViewModelTest.kt
+++ app/src/test/java/com/daniil/shevtsov/idle/core/navigation/ScreenHostViewModelTest.kt
@@ -1,7 +1,15 @@
 package com.daniil.shevtsov.idle.core.navigation
 
+import app.cash.turbine.test
+import assertk.all
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isInstanceOf
+import assertk.assertions.prop
 import com.daniil.shevtsov.idle.MainCoroutineExtension
 import com.daniil.shevtsov.idle.feature.main.data.MainImperativeShell
+import com.daniil.shevtsov.idle.feature.menu.presentation.MenuViewState
+import com.daniil.shevtsov.idle.feature.player.species.domain.Species
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
@@ -12,7 +20,18 @@ class ScreenHostViewModelTest {
     private val viewModel = ScreenHostViewModel(imperativeShell = MainImperativeShell())
 
     @Test
-    fun `should something`() = runBlockingTest {
-        viewModel
+    fun `should show menu screen initially`() = runBlockingTest {
+        viewModel.state.test {
+            assertThat(awaitItem())
+                .all {
+                    prop(ScreenHostViewState::speciesId).isEqualTo(Species.Devourer.id)
+                    prop(ScreenHostViewState::contentState)
+                        .isInstanceOf(ScreenContentViewState.Menu::class)
+                        .prop(ScreenContentViewState.Menu::state)
+                        .all {
+                            prop(MenuViewState::title).isEqualTo("Mutant Idle")
+                        }
+                }
+        }
     }
 }
