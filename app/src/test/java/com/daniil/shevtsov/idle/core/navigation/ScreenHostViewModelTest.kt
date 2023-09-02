2b20802ae896b47dd1390420f55873fe786db522
Daniil Shevtsov <me@daniilshevtsov.com>
2023-09-02 12:54:43 +0400

    add test for action

--- app/src/test/java/com/daniil/shevtsov/idle/core/navigation/ScreenHostViewModelTest.kt
+++ app/src/test/java/com/daniil/shevtsov/idle/core/navigation/ScreenHostViewModelTest.kt
@@ -3,11 +3,15 @@ package com.daniil.shevtsov.idle.core.navigation
 import app.cash.turbine.test
 import assertk.all
 import assertk.assertThat
+import assertk.assertions.containsAll
 import assertk.assertions.isEqualTo
 import assertk.assertions.isInstanceOf
 import assertk.assertions.prop
 import com.daniil.shevtsov.idle.MainCoroutineExtension
+import com.daniil.shevtsov.idle.feature.gamestart.presentation.GameStartViewAction
 import com.daniil.shevtsov.idle.feature.main.data.MainImperativeShell
+import com.daniil.shevtsov.idle.feature.main.presentation.MainViewState
+import com.daniil.shevtsov.idle.feature.main.presentation.SectionKey
 import com.daniil.shevtsov.idle.feature.menu.presentation.MenuViewState
 import com.daniil.shevtsov.idle.feature.player.species.domain.Species
 import kotlinx.coroutines.test.runBlockingTest
@@ -31,6 +35,26 @@ class ScreenHostViewModelTest {
                         .all {
                             prop(MenuViewState::title).isEqualTo("Mutant Idle")
                         }
+                }
+        }
+    }
+
+    @Test
+    fun `should open main screen when start game clicked`() = runBlockingTest {
+        viewModel.state.test {
+            viewModel.handleAction(ScreenViewAction.Start(GameStartViewAction.StartGame))
+
+            assertThat(expectMostRecentItem())
+                .prop(ScreenHostViewState::contentState)
+                .isInstanceOf(ScreenContentViewState.Main::class)
+                .prop(ScreenContentViewState.Main::state)
+                .isInstanceOf(MainViewState.Success::class)
+                .all {
+                    prop(MainViewState.Success::sectionCollapse).containsAll(
+                        SectionKey.Resources to false,
+                        SectionKey.Actions to false,
+                        SectionKey.Upgrades to false,
+                    )
                 }
         }
     }
